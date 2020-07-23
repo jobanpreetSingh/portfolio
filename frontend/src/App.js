ca6e36633bae3b466b1933c27509f4733a86b5aa
jobanpreetsingh <jobandhotimp@yahoo.com>
2020-07-23 00:04:40 -0400

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter,Switch,Route,Redirect } from "react-router-dom";
+import { BrowserRouter,Switch,Route} from "react-router-dom";
 import login from './Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,23 +3,77 @@ import { Card, Button, Form, Row, Col } from "react-bootstrap";
 import './login.css'
 
 export default class login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     email:"",
+     password:""
+    };
+  }
+
+  handleEmail = (event) => {
+   this.setState({ email: event.target.value });
+    console.log(this.state.email)
+  };
+
+  handlePassword = (event) => {
+   this.setState({ password: event.target.value })
+    console.log(this.state.password)
+  }
+
+handleform = (e)=>{
+  e.preventDefault();
+ const{email,password} = this.state
+ if(email,password){
+  return this.callApi();
+
+ }
+ alert('please enter the email or password')
+
+}
+callApi = async () => {
+ await fetch(`http://localhost:7000/login`,{
+body: JSON.stringify(
+  {username:this.state.email,
+password:this.state.password
+})
+
+ }).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          return console.log("error occured", data.error);
+        }
+        console.log(data);
+      
+        // this.setState({ response: data, isLoad: true })
+      });
+    }
+  );
+}
+
   render() {
     return (
         <div className='topMargin'>
-      <div class="container my-3 mt-3" align="center">
+      <div className="container my-3 mt-3" align="center">
         <Card border="info" style={{ width: "30rem" }}>
           <Card.Header>
             <b>Login</b>
           </Card.Header>
           <Card.Body>
             <Card.Title>Portfolio</Card.Title>
-            <Form>
+            <Form onSubmit={this.handelform}>
               <Form.Group as={Row} controlId="formHorizontalEmail">
                 <Form.Label column sm={3}>
                   Email
                 </Form.Label>
                 <Col sm={6}>
-                  <Form.Control type="email" placeholder="Email" />
+                  <Form.Control
+                   type="email" 
+                   placeholder="Email" 
+                  //  value={this.state.value}
+                   onChange={this.handleEmail}
+                 />
                 </Col>
               </Form.Group>
               <Form.Group as={Row} controlId="formHorizontalPassword">
@@ -27,7 +81,12 @@ export default class login extends Component {
                   Password
                 </Form.Label>
                 <Col sm={6}>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control
+                   type="password" 
+                   placeholder="Password"
+                  //  value={this.state.value}
+                   onChange={this.handlePassword}
+                    />
                 </Col>
               </Form.Group>
               <Form.Group as={Row}>

2b57c5cfa42acab4c91606db32f5d5fc41ea0374
sameerbagga <sameerbagga@yahoo.com>
2020-07-23 22:26:17 -0400

    login backend modified

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require ('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,11 +13,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- backend/helpers/loginPage.js
+++ backend/helpers/loginPage.js
@@ -12,26 +12,33 @@ function errorHandler(err) {
 
 
 async function loginUser(userData) {
-    return new Promise (async (resolve, reject) => {
-        let userInfo = await dbConn.select('*').from('users').where('username', userData.username);
-        if (userInfo.length !== 0) {
-            if (await argon2.verify(userInfo[0].password, userData.password)) {
-                resolve({
-                    username: userInfo[0].username,
-                    firstName: userInfo[0].firstName,
-                    lastName: userInfo[0].lastName
-                });
+    return new Promise(async (resolve, reject) => {
+        if (!userData.username || !userData.password) {
+            resolve({
+                status: 400,
+                message: 'Both username and Password are required'
+            });
+        } else {
+            let userInfo = await dbConn.select('*').from('users').where('username', userData.username);
+            if (userInfo.length !== 0) {
+                if (await argon2.verify(userInfo[0].password, userData.password)) {
+                    resolve({
+                        username: userInfo[0].username,
+                        firstName: userInfo[0].firstName,
+                        lastName: userInfo[0].lastName
+                    });
+                } else {
+                    resolve({
+                        status: 404,
+                        message: 'Username or Password incorrect'
+                    });
+                }
             } else {
                 resolve({
                     status: 404,
-                    message: 'Username or Password dont match'
+                    message: 'Username or Password incorrect'
                 });
             }
-        } else {
-            resolve({
-                status: 404,
-                message: 'Username or Password incorrect'
-            });
         }
     }).catch(err => {
         console.log('Error from loginUser() --->', err);
